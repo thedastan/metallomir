c2ea5bed519c3b67c8ad2fad4f9d391aef56a024
thedastan <dastanshermatov@gmail.com>
2024-12-17 11:21:08 +0600

    hero

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -1,303 +1,210 @@
 import Description from "@/components/ui/Description";
 import Title from "@/components/ui/texts/Title";
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
-import Image from "next/image";
- 
-import { GoArrowRight, GoArrowUpRight } from "react-icons/go";
-
-import { MdMarkEmailUnread } from "react-icons/md";
 import Link from "next/link";
-
-import img from "@/assets/images/homefigure.png";
+import { GoArrowRight } from "react-icons/go";
 import { PRICE_LIST } from "@/constants/admin";
 
 const Hero = () => {
-	return (
-		<>
-			<Box
-				py={20}
-				p={{ md: "50px 0 0 0", base: "50px 14px 14px 14px" }}
-				position="relative"
-				bg="#ECF0F4"
-				w="100%"
-				h="100%">
-				<Box
-					position="absolute"
-					top="0"
-					bottom="0"
-					left="0"
-					right="0"
-					zIndex="0">
-					<Box
-						position="absolute"
-						top="0"
-						bottom="0"
-					  left={{ md: "25%", base: "-65%" }}
-						width="100px"
-						bg={{ md:"linear-gradient(135deg, transparent, rgb(255, 255, 255) 50%, transparent)", base: "linear-gradient(135deg, transparent, rgb(255, 255, 255) 10%, transparent)" }}
+  return (
+    <Box
+      py={20}
+      position="relative"
+      bg="#ECF0F4"
+      w="100%"
+      h="100vh"
+      overflow="hidden"
+    >
+      {/* Видео как фон */}
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        w="100%"
+        h="100%"
+        zIndex="1"
+        overflow="hidden"
+        _before={{
+          content: `""`,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          w: "100%",
+          h: "100%",
+          bg: "rgba(0, 0, 0, 0.6)", // Затемнение
+          zIndex: 2,
+        }}
+      >
+        <video
+          autoPlay
+          loop
+          muted
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        >
+          <source src="/metal.mp4" type="video/mp4" />
+        </video>
+      </Box>
 
-						filter="blur(20px)"
-						transform="skewX(-40deg)"
-					/>
-					<Box
-						position="absolute"
-						top="0"
-						bottom="0"
-						right={{ md: "55%", base: "65%" }}
-						width="100px"
-						bg={{ md:"linear-gradient(135deg, transparent, rgb(255, 255, 255) 60%, transparent)", base: "linear-gradient(135deg, transparent, rgb(255, 255, 255) 10%, transparent)" }}
+      {/* Централизованный контент */}
+      <Flex
+        position="relative"
+        zIndex="10"
+        h="100%"
+        alignItems={{ base: "flex-start", md: "center" }} // Выровнять по началу на мобильных
+        justifyContent={{ base: "flex-start", md: "center" }} // Выровнять по началу на мобильных
+        textAlign={{ base: "left", md: "center" }} // Текст слева на мобильных
+        flexDirection="column"
+        px={{ base: 6, md: 4 }} // Добавить отступы для мобильной версии
+        py={{ base: 10, md: 0 }} // Отступ сверху для мобильной версии
+      >
+        <Title
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          fontFamily="Gilroy"
+          fontWeight={500}
+          fontSize={{ md: 68, base: 32 }}
+          lineHeight={{ md: "84px", base: "42px" }}
+          color="white"
+        >
+          Металлопродукция от лидеров рынка
+        </Title>
 
-						filter="blur(20px)"
-						transform="skewX(-40deg)"
-					/>
-				</Box>
-				<Box py={10} position="relative" zIndex="10" className="container">
-					<Flex
-						flexDirection={{ md: "row", base: "column" }}
-						justifyContent="space-between"
-						alignItems={{ md: "center", base: "start" }}
-						gap={{ md: 20, base: 6 }}>
-						<Flex
-							w={{ md: 730, base: "100%" }}
-							alignItems="start"
-							flexDir="column"
-							gap={6}>
-							<Title
-								data-aos="fade-up"
-								data-aos-duration="1000"
-								fontFamily="Gilroy"
-								fontWeight={500}
-								fontSize={{ md: 68, base: 32 }}
-								lineHeight={{ md: "84px", base: "32px" }}>
-								Металлопродукция от лидеров рынка
-							</Title>
-							<Description
-								data-aos="fade-up"
-								data-aos-duration="1300"
-								fontSize={{ md: 20, base: 18 }}>
-								Широкий ассортимент металлопроката по доступным ценам. Быстрая
-								доставка по всему Кыргызстану.
-							</Description>
+        <Description
+          data-aos="fade-up"
+          data-aos-duration="1300"
+          fontSize={{ md: 20, base: 18 }}
+          mt={4}
+          color="white"
+        >
+          Широкий ассортимент металлопроката по доступным ценам. Быстрая доставка по всему Кыргызстану.
+        </Description>
 
-							<Box
-								data-aos="fade-up"
-								data-aos-duration="1700"
-								display={{ md: "flex", base: "none" }}
-								p="0 10px "
-								flexDirection="column"
-								gap={1}
-								borderLeft="solid 1px black">
-								<Description fontSize={16}>Для строительства</Description>
-								<Description fontSize={16}>Производства</Description>
-								<Description fontSize={16}>Торговли</Description>
-							</Box>
+        <Flex
+          data-aos="fade-up"
+          data-aos-duration="1700"
+          flexDirection={{ base: "column", md: "row" }}
+          gap={4}
+          mt={6}
+          alignItems={{ base: "flex-start", md: "center" }}
+          justifyContent={{ base: "flex-start", md: "center" }}
+        >
+          <Description
+            fontSize={16}
+            color="white"
+            display="inline-block"
+            position="relative"
+            _before={{
+              content: `'•'`, // Точка перед текстом
+              position: 'absolute',
+              left: '-10px', // Позиционируем точку слева
+              top: '50%',
+              transform: 'translateY(-50%)',
+              color: 'white',
+            }}
+          >
+            Для строительства
+          </Description>
 
-							<Flex gap={6}>
-								<Link href={"/table"}>
-									<Button
-										data-aos="fade-up"
-										data-aos-duration="2000"
-										display={{ md: "flex", base: "none" }}
-										w={220}
-										h="56px"
-										borderRadius={50}
-										color="black"
-										p={4}
-										border="solid 1px black"
-										bg="none">
-										<Text>Ассортименты</Text>
-										<Text>
-											<GoArrowRight />
-										</Text>
-									</Button>
-								</Link>
+          <Description
+            fontSize={16}
+            color="white"
+            display="inline-block"
+            position="relative"
+            _before={{
+              content: `'•'`, // Точка перед текстом
+              position: 'absolute',
+              left: '-7px',
+              top: '50%',
+              transform: 'translateY(-50%)',
+              color: 'white',
+            }}
+          >
+            Производства
+          </Description>
 
-								<Link href={PRICE_LIST} target={"_blank"}>
-									<Button
-										data-aos="fade-up"
-										data-aos-duration="2000"
-										display={{ md: "flex", base: "none" }}
-										w={220}
-										h="56px"
-										borderRadius={50}
-										color="black"
-										p={4}
-										border="solid 1px black"
-										bg="none">
-										<Text>Скачать Прайс-Лист</Text>
+          <Description
+            fontSize={16}
+            color="white"
+            display="inline-block"
+            position="relative"
+            _before={{
+              content: `'•'`, // Точка перед текстом
+              position: 'absolute',
+              left: '-7px',
+              top: '50%',
+              transform: 'translateY(-50%)',
+              color: 'white',
+            }}
+          >
+            Торговли
+          </Description>
+        </Flex>
 
-										<Text>
-											<GoArrowRight />
-										</Text>
-									</Button>
-								</Link>
-							</Flex>
-						</Flex>
+        <Box
+          mt={8}
+          display="flex"
+          flexDirection={{ base: "column", md: "row" }} // Вертикально на мобильных, горизонтально на десктопах
+          gap={6}
+          alignItems={{ base: "stretch", md: "center" }} // Растягивание кнопок на мобильных
+          justifyContent={{ md: "center" }} // Центрирование на десктопах
+        >
+          <Link href={"/table"}>
+            <Button
+              data-aos="fade-up"
+              data-aos-duration="2000"
+              w={{ base: "100%", md: 220 }} // Полная ширина на мобильных, фиксированная на десктопах
+              h="56px"
+              borderRadius={50}
+              color="white"
+              p={4}
+              border="solid 1px white"
+              bg="none"
+              transition="all 0.3s ease" // Плавный переход для фона и цвета текста
+              _hover={{
+                bg: "white",  // Белый фон при ховере
+                color: "black",  // Черный текст при ховере
+                borderColor: "white",  // Белая граница
+              }}
+            >
+              <Text>Ассортименты</Text>
+              <Text>
+                <GoArrowRight />
+              </Text>
+            </Button>
+          </Link>
 
-						<Box w="100%" data-aos="fade-up" position="relative">
-							<Box
-								data-aos-duration="1200"
-								display={{ md: "flex", base: "none" }}>
-								<Box
-									position="relative"
-									w="580px"
-									h="580px"
-									borderRadius={20}
-									overflow="hidden">
-									<video
-										autoPlay
-										loop
-										muted
-										style={{
-											position: "absolute",
-											top: 0,
-											left: 0,
-											width: "100%",
-											height: "100%",
-											objectFit: "cover",
-											zIndex: -1,
-										}}>
-										<source src="/metal.mp4" type="video/mp4" />
-									</video>
-								</Box>
-							</Box>
-
-							<Box
-								data-aos="zoom-in"
-								data-aos-duration="1700"
-								w="100%"
-								display={{ md: "none", base: "flex" }}>
-								<Box
-									position="relative"
-									w="100%"
-									h="300px"
-									borderRadius={20}
-									overflow="hidden">
-									<video
-										autoPlay
-										loop
-										muted
-										style={{
-											position: "absolute",
-											top: 0,
-											left: 0,
-											width: "100%",
-											height: "100%",
-											objectFit: "cover",
-											zIndex: -1,
-										}}>
-										<source src="/metal.mp4" type="video/mp4" />
-									</video>
-								</Box>
-							</Box>
-
-							<Flex
-								position="absolute"
-								// p={6}
-								borderRadius="10px"
-								gap={{ md: 2, base: 1 }}
-								mt={{ md: "-120px", base: "-87px" }}
-								ml="-28px">
-								<Box
-									p="0"
-									position="absolute"
-									w={{ md: 400, base: 360 }}
-									h={{ md: "140px", base: "100px" }}>
-									<Image
-										style={{ width: "100%", height: "100%" }}
-										src={img}
-										alt="img"
-									/>
-								</Box>
-								<Flex
-									ml={{ md: 14, base: 10 }}
-									gap={2}
-									mt={{ md: 14, base: 10 }}
-									position="absolute">
-									<Link href={"/catalog"}>
-										<Button
-											// position="absolute"
-											fontSize="14px"
-											w={{ md: 210, base: 190 }}
-											h={{ md: "54px", base: "46px" }}
-											borderRadius={50}
-											color="white">
-											Каталог продукций
-										</Button>
-									</Link>
-									<Link href={"/catalog"}>
-										<Button
-											// position="absolute"
-											w={{ md: "54px", base: "46px" }}
-											h={{ md: "54px", base: "46px" }}
-											borderRadius={50}
-											color="white">
-											<GoArrowUpRight />
-										</Button>
-									</Link>
-								</Flex>
-							</Flex>
-						</Box>
-
-						<Box
-							display={{ md: "none", base: "flex" }}
-							data-aos="fade-up"
-							data-aos-duration="1700"
-							alignItems="start"
-							justifyContent="start"
-							p="0 10px "
-							flexDirection="column"
-							gap={1}
-							borderLeft="solid 1px black">
-							<Description fontSize={16}>Для строительства</Description>
-							<Description fontSize={16}>Производства</Description>
-							<Description fontSize={16}>Торговли</Description>
-						</Box>
-
-						 <Flex w="100%" flexDir="column" gap={6}>
-						 <Link href={PRICE_LIST} target={"_blank"}>
-							<Button
-								data-aos="fade-up"
-								data-aos-duration="2000"
-								display={{ md: "none", base: "flex" }}
-								w="100%"
-								h="56px"
-								borderRadius={50}
-								color="black"
-								p={4}
-								border="solid 1px black"
-								bg="none">
-								<Text>Скачать Прайс-Лист</Text>
-
-								<Text>
-									<GoArrowRight />
-								</Text>
-							</Button>
-						</Link>
-						<Link href={"/table"}>
-							<Button
-								data-aos="fade-up"
-								data-aos-duration="2000"
-								display={{ md: "none", base: "flex" }}
-								w="100%"
-								h="56px"
-								borderRadius={50}
-								color="black"
-								p={4}
-								border="solid 1px black"
-								bg="none">
-								<Text>Ассортименты</Text>
-								<Text>
-									<GoArrowRight />
-								</Text>
-							</Button>
-						</Link>
-						 </Flex>
-					</Flex>
-				</Box>
-			</Box>
-		</>
-	);
+          <Link href={PRICE_LIST} target={"_blank"}>
+            <Button
+              data-aos="fade-up"
+              data-aos-duration="2000"
+              w={{ base: "100%", md: 220 }} // Полная ширина на мобильных, фиксированная на десктопах
+              h="56px"
+              borderRadius={50}
+              color="white"
+              p={4}
+              border="solid 1px white"
+              bg="none"
+              transition="all 0.3s ease" // Плавный переход для фона и цвета текста
+              _hover={{
+                bg: "white",  // Белый фон при ховере
+                color: "black",  // Черный текст при ховере
+                borderColor: "white",  // Белая граница
+              }}
+            >
+              <Text>Скачать Прайс-Лист</Text>
+              <Text>
+                <GoArrowRight />
+              </Text>
+            </Button>
+          </Link>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Hero;
